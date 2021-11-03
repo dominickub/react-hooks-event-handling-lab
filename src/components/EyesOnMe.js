e51cea0287520b41ba77fedc73a7fa43e2d59e84
dominick <dominickugas@gmail.com>
2021-11-02 22:39:18 -0400

    commit tree 556a49fb166b56cb0dceaac8924d532cfef3785d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function eyesOn(event){
+        console.log("Good!")
+    }
+    function lookAtMe(event){
+        console.log("Hey! Eyes on me!")
+    }
+   return (<button onFocus={eyesOn} onBlur={lookAtMe}>Eyes On Me</button>)
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+function Keypad(){
+    function handleChange(event){
+        console.log("Entering password...");
+    }
+
+return( 
+    <input 
+        type="password"
+        onChange = {handleChange} />
+)}
+
+export default Keypad
